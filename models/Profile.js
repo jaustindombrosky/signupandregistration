dd38f8d19cabef2b104b99182f8be47688aee861
jaustindombrosky <austindombrosky@gmail.com>
2018-07-20 14:00:01 -0600

    Profile.js schema

---
+++ models/Profile.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose')
+
+var ProfileSchema = new mongoose.Schema({
+    email: {type:String, default:""},
+    password: {type:String, default:""},
+    timestamp: {type:Date, default: Date.now}
+
+})
+
+module.exports = mongoose.model('ProfileSchema', ProfileSchema)
