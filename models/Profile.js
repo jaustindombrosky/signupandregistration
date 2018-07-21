52be68806070c818b9d431669150c06ae050e761
jaustindombrosky <austindombrosky@gmail.com>
2018-07-21 07:47:50 -0600

    get and post routes

--- models/Profile.js
+++ models/Profile.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose')
 
 var ProfileSchema = new mongoose.Schema({
     email: {type:String, default:""},
+    username: {type:String, default:""},
     password: {type:String, default:""},
     timestamp: {type:Date, default: Date.now}
 
--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,38 @@ var express = require('express')
 var router = express.Router()
 var Profile = require('../models/Profile')
 
-router.get('/:resource', function (req, res, next) {
+router.post('/:resource', function (req, res, next){
+    var resource = req.params.resource
+    if (resource == 'profile'){ //create profile
 
+        var formData = req.body
+        Profile.create(formData, function(err, profile){
+            if (err){
+                res.json({
+                    confirmation: "fail",
+                    message: err
+                })
+
+                return
+            }
+
+            res.json({
+                confirmation: 'success',
+                result: profile
+            })
+
+        })
+
+        return 
+
+    }
+
+    res.json({
+        confirmation: 'fail',
+        message: 'Resource '+resource+' not supported.'
+    })
+})
+router.get('/:resource', function (req, res, next) {
     var resource = req.params.resource
     if (resource == 'profile') { //request for profile
     Profile.find(null, function (err, profiles) {
@@ -29,6 +59,43 @@ router.get('/:resource', function (req, res, next) {
         confirmation: 'fail',
         message: 'Resource '+resource+' not supported.'
 
+    })
+})
+
+router.get('/:resource/:id', function(req, res, next){
+    var resource = req.params. resource
+    var id = req.params.id
+
+    if (resource == 'profile'){ //fetch a particular profile
+        Profile.findById(id, function(err, profile){
+            if (err){
+                res.json({
+                    confirmation: 'fail',
+                    message: 'Profile not found'
+                })
+                return
+            }
+
+            if (profile == null){
+                res.json({
+                    confirmation: 'fail',
+                    message: 'Profile not found'
+                })
+                return
+            }
+            res.json({
+                confirmation: 'success',
+                result: profile
+            })
+
+        })
+
+        return
+
+    }
+    res.json({
+        confirmation: 'fail',
+        message: 'Resource '+resource+' not supported.'
     })
 })
 module.exports = router
